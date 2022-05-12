b7ec2c9ddba19f47cb4db7d58f716699ee08e7d6
qyleng <105409085+qyleng@users.noreply.github.com>
2022-05-12 15:07:17 +0800

    my first upload of my tour

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.3252470669900163,
+        "pitch": 0.0173075653471777,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.642149768186714,
+          "pitch": 0.07597637992709849,
+          "rotation": 0.7853981633974483,
+          "target": "2-along-engineering-drive-1"
+        },
+        {
+          "yaw": -3.1368841687039826,
+          "pitch": 0.07757032268272646,
+          "rotation": 0,
+          "target": "0-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.516093214466082,
+          "pitch": -0.2834068023663896,
+          "title": "Spinelli Coffee Company",
+          "text": "the only eating place at EA"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.15672344303622765,
+        "pitch": -0.18296569081306657,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1480346088436839,
+          "pitch": 0.0005243424452814338,
+          "rotation": 6.283185307179586,
+          "target": "0-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4202958401699188,
+          "pitch": -0.6578897483841821,
+          "title": "Sculpture",
+          "text": "of something"
+        }
+      ]
+    },
+    {
+      "id": "2-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.27960886996235246,
+        "pitch": 0.2620859895430243,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0023800079983225,
+          "pitch": -0.07085274541105235,
+          "rotation": 0.7853981633974483,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.18248028037357322,
+          "pitch": -0.15303611565957098,
+          "title": "Welcome",
+          "text": "to the faculty of Engineering"
+        }
+      ]
+    }
+  ],
+  "name": "Engineering ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
